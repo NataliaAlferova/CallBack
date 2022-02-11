0192b033fbde358169d83a63096f89232f184cc7
Natalia Alferova <natalia@alferova.me>
2022-02-11 11:25:06 +0100

    Version 3

--- src/test/java/CallBackTest.java
+++ src/test/java/CallBackTest.java
@@ -23,7 +23,7 @@ public class CallBackTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
 
 
