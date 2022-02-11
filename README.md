6dc53f264ae8a2fed5d0c7d0edfab3e804a99527
Natalia Alferova <natalia@alferova.me>
2022-02-11 09:43:21 +0100

    Version 1

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/qtq9oyjyhgyeylp1?svg=true)](https://ci.appveyor.com/project/NataliaAlferova/callback)
--- src/test/java/CallBackTest.java
+++ src/test/java/CallBackTest.java
@@ -1,25 +1,48 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallBackTest {
     private WebDriver driver;
 
     @BeforeAll
     public static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", ".driver/mac/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "driver/mac/chromedriver");
     }
 
     @BeforeEach
-    public void setUp () {
-        ChromeOptions options = new ChromeOptions();
+    public void setUp() {
+//        ChromeOptions options = new ChromeOptions();
 //        options.addArguments("--disable-dev-shm-usage");
 //        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+//        options.addArguments("--headless");
+        driver = new ChromeDriver();
     }
 
 
+    @Test
+    public void shouldSendForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Александр");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String expectedText = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+
+        assertEquals(expectedText, actualText);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
 }
